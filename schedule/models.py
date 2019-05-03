46e77b800d4809e5ef9aa93bcf32e27a97bec2d1
Ryan Collins <ryan@ohmgeek.co.uk>
2019-05-03 11:54:19 +0100

    Basic schedule event created, used for creating ents for a given event

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ScheduleEvent(models.Model):
+    name = models.CharField(
+            blank=True,
+            max_length=20,
+            help_text="Please enter the name of the ent being held at your event"
+        )
+
+    description = models.TextField(
+        blank=False
+    )
+
+    # todo: Need to add location tie-ins (as foreign key from separate app).
+
+    # todo: need to add images for the event, if required.
